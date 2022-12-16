10d5d88e2e037a3fc7eb49ef2ea399837cbb6e8f
Panda115 <panda.dev1115@gmail.com>
2022-12-16 10:57:05 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-test02
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-evcjnc)
---
+++ component/Test.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function Car() {
+  return <h1>This is a React Test Code.</h1>;
+}
